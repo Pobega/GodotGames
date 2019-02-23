25ec439bca7ee3b24dbeac6174b4e8d56eba31c4
Michael Pobega <pobega@gmail.com>
2019-02-23 13:25:12 -0500

    Only allow keyboard events or joypad events to get past title screen

--- Jumptest/src/ui/TitleScreen.gd
+++ Jumptest/src/ui/TitleScreen.gd
@@ -4,7 +4,7 @@ func _ready():
 	$FadeController/Fader.play("fade_in")
 
 func _input(event):
-	if event.is_pressed():
+	if event is InputEventKey or event is InputEventJoypadButton:
 		disable_input(true) # Disable input so we can't spam buttons
 		
 		# Block until the fade_out animation is done
