7f066b713709e1931c38b6ab4c1af0f838708099
Michael Pobega <pobega@gmail.com>
2019-02-23 00:33:10 -0500

    Fixed HTML5 resizing

--- Jumptest/src/game/Screen.gd
+++ Jumptest/src/game/Screen.gd
@@ -5,9 +5,11 @@ extends Node
 # var b = "textvar"
 
 func _ready():
-#	if OS.is_debug_build():
-#		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -1000)
-#		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true)
+	if OS.is_debug_build():
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), -1000)
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Music"), true)
+	if OS.get_name()=="HTML5":
+		OS.set_window_maximized(true)
 	pass
 
 #func _process(delta):
