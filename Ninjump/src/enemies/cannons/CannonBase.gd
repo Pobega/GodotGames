ce82d06bfdd4cb77c73716bbb46f8531217967c3
Michael Pobega <pobega@gmail.com>
2019-02-26 21:54:54 -0500

    CannonBase scene: add FirstShotTimer for initial shot
    This lets us specify a 'shot time' of something like 3 seconds, but allows us to
    take the first shot 1 second after spawning. Without this there is an awkward
    pause before the initial cannonball shows.

--- Ninjump/src/enemies/cannons/CannonBase.gd
+++ Ninjump/src/enemies/cannons/CannonBase.gd
@@ -3,15 +3,24 @@ extends StaticBody2D
 export(int, -1, 1) var vertical = 0
 export(int, -1, 1) var horizontal = 0
 
-export(float) var wait_time = 3.0
+export(float) var wait_time = 2.0
+export(float) var first_shot_time = 1.0
 
 const CANNONBALL = preload("res://src/enemies/cannons/Cannonball.tscn")
 
 func _ready():
+	$FirstShotTimer.set_wait_time(first_shot_time)
+	$FirstShotTimer.start()
+	
+func _on_FirstShotTImer_timeout():
 	$ShootTimer.set_wait_time(wait_time)
 	$ShootTimer.start()
-
+	shoot_cannonball()
+	
 func _on_ShootTimer_timeout():
+	shoot_cannonball()
+	
+func shoot_cannonball():
 	var cannonball = CANNONBALL.instance()
 	cannonball.horizontal = horizontal
 	cannonball.vertical = vertical
