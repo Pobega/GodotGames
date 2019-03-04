62adfbd6286ef75c593eed7551cc0274235cad0a
Michael Pobega <pobega@gmail.com>
2019-03-03 20:11:57 -0500

    Add location based bouncing

--- Pong/Ball.gd
+++ Pong/Ball.gd
@@ -22,21 +22,43 @@ func _physics_process(delta):
 func handle_collision(collision):
 	velocity = velocity.bounce(collision.normal)
 	if "Player" in collision.collider.get_name():
-		velocity.y = velocity.rotated(sign(velocity.x) * sign(collision.collider.velocity.y)).y
-		velocity.x *= speed_step
-		if abs(velocity.x) > MAX_SPEED:
-			velocity.x = MAX_SPEED * sign(velocity.x)
-		print(velocity.x)
+		bounce_off_paddle(collision.collider)
+		speed_up()
+
+func rotate_direction(dir, rad):
+	return velocity.rotated(sign(velocity.x) * sign(dir) * rad).y
+
+func speed_up():
+	velocity.x *= speed_step
+	if abs(velocity.x) > MAX_SPEED:
+		velocity.x = MAX_SPEED * sign(velocity.x)
+
+func bounce_off_paddle(collider):
+	positional_bounce(collider)
+	velocity_bounce(collider)
+
+func positional_bounce(collider):
+	var contact_point = collider.position.y - position.y
+	if abs(contact_point) < 10:
+		return
+	elif abs(contact_point) < 30:
+		velocity.y = rotate_direction(contact_point * -1, 0.4)
+	else:
+		velocity.y = rotate_direction(contact_point * -1, 0.7)
+
+func velocity_bounce(collider):
+	if collider.get("velocity"):
+		velocity.y = rotate_direction(collider.velocity.y,  0.2)
 
 func _on_VisibilityNotifier2D_screen_exited():
 	emit_signal("destroyed")
 	queue_free()
 
 func _on_Timer_timeout():
-	randomize()
-	if randi() % 2:
-		velocity = Vector2(speed, 0)
-	else:
-		velocity = Vector2(-speed, 0)
-	velocity.y = velocity.rotated(deg2rad(randi() % 180 - 90)).y
-	print(velocity)
+	velocity = Vector2(-speed, 0)
+#	randomize()
+#	if randi() % 2:
+#		velocity = Vector2(speed, 0)
+#	else:
+#		velocity = Vector2(-speed, 0)
+#	velocity.y = velocity.rotated(deg2rad(randi() % 180 - 90)).y
