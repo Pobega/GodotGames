a7f105d421371af41d8988ed3e28ad8f4b77c418
Michael Pobega <pobega@gmail.com>
2019-03-04 02:22:57 -0500

    Straightened out edges

--- Minigolf/scenes/ball/Ball.gd
+++ Minigolf/scenes/ball/Ball.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export(float) var drag = 0.97
+export(float) var drag = 0.99
 
 var impulse_start
 var velocity = Vector2()
@@ -21,8 +21,7 @@ func _physics_process(delta):
 	if collision:
 		velocity = velocity.bounce(collision.normal)
 	velocity = velocity*drag
-	if velocity.abs() < Vector2(1, 1) and state == STATE.MOVING:
-		print("stopping")
+	if velocity.abs().x < 1 and velocity.abs().y < 1 and state == STATE.MOVING:
 		state = STATE.STOPPED
 		velocity = Vector2()
 
@@ -39,6 +38,5 @@ func screen_clamp(screen_position):
 	return screen_position
 
 func hit_ball(start, end):
-	print(end)
-	velocity = (start - end)*2
+	velocity = (start - end)*4
 	state = STATE.MOVING
