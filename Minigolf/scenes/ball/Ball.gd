526dc97a10c4b97525f0b0ecaefed29b9a44560a
Michael Pobega <pobega@gmail.com>
2019-03-04 02:04:13 -0500

    Move to Kinematic body

--- Minigolf/scenes/ball/Ball.gd
+++ Minigolf/scenes/ball/Ball.gd
@@ -1,14 +1,44 @@
-extends RigidBody2D
+extends KinematicBody2D
+
+export(float) var drag = 0.97
 
 var impulse_start
+var velocity = Vector2()
+
+enum STATE {MOVING, STOPPED}
+var state = STATE.STOPPED
+
 
 func _unhandled_input(event):
+	if state != STATE.STOPPED: return
 	if event.is_action_pressed("putt"):
-		print("Held at: ", event.position)
-		impulse_start = event.position
+		impulse_start = screen_clamp(event.position)
 	if event.is_action_released("putt"):
-		print("Released at: ", event.position)
-		hit_ball(impulse_start, event.position)
+		hit_ball(impulse_start, screen_clamp(event.position))
+
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.normal)
+	velocity = velocity*drag
+	if velocity.abs() < Vector2(1, 1) and state == STATE.MOVING:
+		print("stopping")
+		state = STATE.STOPPED
+		velocity = Vector2()
+
+func screen_clamp(screen_position):
+	var screen_size = get_viewport().get_visible_rect().size
+	if screen_position.x < 0:
+		screen_position.x = 0
+	elif screen_position.x > screen_size.x:
+		screen_position.x = screen_size.x
+	if screen_position.y < 0:
+		screen_position.y = 0
+	elif screen_position.y > screen_size.y:
+		screen_position.y = screen_size.y
+	return screen_position
 
 func hit_ball(start, end):
-	apply_impulse(Vector2(), (start - end)*2)
+	print(end)
+	velocity = (start - end)*2
+	state = STATE.MOVING
